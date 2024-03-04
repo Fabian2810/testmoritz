135087e4657a8d843ac25344b5eba8f5ea7f4afc
Fabian Böhnke <120092627+Fabian2810@users.noreply.github.com>
2024-03-04 10:00:34 +0100

    erster commit

---
+++ cicdscriptuebung/src/main/java/de/volkswagen/moritz/cicdscriptuebung/CicdscriptuebungApplication.java
@@ -0,0 +1,13 @@
+package de.volkswagen.moritz.cicdscriptuebung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdscriptuebungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdscriptuebungApplication.class, args);
+	}
+
+}
